5b1612f0dee201370ac694c68014a000903d1152
MaitreRouge <39885214+MaitreRouge@users.noreply.github.com>
2019-09-08 22:36:51 +0200

    Clean du code

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 const Discord = require("discord.js");
 const request = require("request-promise")
-//const fs = require("fs");
 const bot = new Discord.Client();
 const config = require("./botconfig.json");
-//bot.commands = new Discord.Collection();
 let prefix = ("EF!");
-
-/*fs.readdir("./commandes/", (err, files) => {
-    if(err) console.log(err);
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0){
-        console.log("Les fichiers commandes n'ont pas été trouvés");
-        return;
-    }
-
-    jsfile.forEach((f, i) =>{
-        let props = require(`./commandes/${f}`);
-        console.log(`${f} a bien été chargé`)
-        bot.commands.set(props.help.name, props);
-    });
-});*/
 
 bot.login(config.token)
 const key = config.api_key
@@ -37,9 +19,6 @@ bot.on ("message", message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
-    /*let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(commandfile) commandfile.run(bot,message,args,key);*/
 
     if(cmd === (prefix + "ping")){
         message.channel.send("Pong")
