96c25cd7a68a2a39f70963859c53f71ed5bb830d
Romain J <romain.ordi@gmail.com>
2019-10-28 16:13:05 +0100

    fix(commands|aliases): fix aliases
    refactor(config): remove unused config's options

--- utils/process_commands.js
+++ utils/process_commands.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
         const args = message.content.slice(client.config.defaultSettings.prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
-        const cmd = client.commands.get(command);
+        const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
         if (!cmd) return;
 
         await cmd.run(client, message, args)
